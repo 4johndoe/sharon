0f2a1cab844fe4b08dbb0d377e126ff960e84281
4johndoe <bogdandemchenko@gmail.com>
2016-01-11 16:17:09 +0200

    css..

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <link rel="pingback" href="http://www.sharon.com.ua/wp/xmlrpc.php">
 
     <script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>
-<title>sharon | Just another WordPress sitesharon | Just another WordPress site</title>
-<link rel="alternate" type="application/rss+xml" title="sharon &raquo; Лента" href="http://www.sharon.com.ua/wp/feed/" />
-<link rel="alternate" type="application/rss+xml" title="sharon &raquo; Лента комментариев" href="http://www.sharon.com.ua/wp/comments/feed/" />
+<title>SHARON GROUP LLC</title>
+<!-- <link rel="alternate" type="application/rss+xml" title="sharon &raquo; Лента" href="http://www.sharon.com.ua/wp/feed/" />
+<link rel="alternate" type="application/rss+xml" title="sharon &raquo; Лента комментариев" href="http://www.sharon.com.ua/wp/comments/feed/" /> -->
 		<script type="text/javascript">
 			window._wpemojiSettings = {"baseUrl":"http:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"http:\/\/www.sharon.com.ua\/wp\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.3.1"}};
 			!function(a,b,c){function d(a){var c=b.createElement("canvas"),d=c.getContext&&c.getContext("2d");return d&&d.fillText?(d.textBaseline="top",d.font="600 32px Arial","flag"===a?(d.fillText(String.fromCharCode(55356,56812,55356,56807),0,0),c.toDataURL().length>3e3):(d.fillText(String.fromCharCode(55357,56835),0,0),0!==d.getImageData(16,16,1,1).data[0])):!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g;c.supports={simple:d("simple"),flag:d("flag")},c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.simple&&c.supports.flag||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
@@ -31,16 +31,16 @@ img.emoji {
 	padding: 0 !important;
 }
 </style>
-<link rel='stylesheet' id='contact-form-7-css'  href='http://localhost/sharon/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.3.1' type='text/css' media='all' />
-<link rel='stylesheet' id='pixova-lite-min-style-css'  href='http://localhost/sharon/wp-content/themes/pixova-lite/style.css?ver=4.3.1' type='text/css' media='all' />
-<link rel='stylesheet' id='animate-min-css-css'  href='http://localhost/sharon/wp-content/themes/pixova-lite/layout/css/animate.min.css?ver=4.3.1' type='text/css' media='all' />
-<link rel='stylesheet' id='font-awesome-min-css-css'  href='http://localhost/sharon/wp-content/themes/pixova-lite/layout/css/font-awesome.min.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='contact-form-7-css'  href='wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='pixova-lite-min-style-css'  href='wp-content/themes/pixova-lite/style.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='animate-min-css-css'  href='wp-content/themes/pixova-lite/layout/css/animate.min.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='font-awesome-min-css-css'  href='wp-content/themes/pixova-lite/layout/css/font-awesome.min.css?ver=4.3.1' type='text/css' media='all' />
 <link rel='stylesheet' id='ga-fonts-css'  href='//fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C600%2C700%7CSource+Sans+Pro%3A300%2C400%2C600%2C700%7CInconsolata%3A400%2C700&#038;subset=latin%2Clatin-ext&#038;ver=4.3.1' type='text/css' media='all' />
-<link rel='stylesheet' id='bootstrap-min-css-css'  href='http://localhost/sharon/wp-content/themes/pixova-lite/layout/css/bootstrap.min.css?ver=4.3.1' type='text/css' media='all' />
-<link rel='stylesheet' id='owlCarousel-main-css-css'  href='http://localhost/sharon/wp-content/themes/pixova-lite/layout/css/owl.carousel.css?ver=4.3.1' type='text/css' media='all' />
-<link rel='stylesheet' id='owlCarousel-theme-css-css'  href='http://localhost/sharon/wp-content/themes/pixova-lite/layout/css/owl.theme.css?ver=4.3.1' type='text/css' media='all' />
-<script type='text/javascript' src='http://localhost/sharon/wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
-<script type='text/javascript' src='http://localhost/sharon/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<link rel='stylesheet' id='bootstrap-min-css-css'  href='wp-content/themes/pixova-lite/layout/css/bootstrap.min.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='owlCarousel-main-css-css'  href='wp-content/themes/pixova-lite/layout/css/owl.carousel.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='owlCarousel-theme-css-css'  href='wp-content/themes/pixova-lite/layout/css/owl.theme.css?ver=4.3.1' type='text/css' media='all' />
+<script type='text/javascript' src='wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
+<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
 <!-- <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.sharon.com.ua/wp/xmlrpc.php?rsd" />
 <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.sharon.com.ua/wp/wp-includes/wlwmanifest.xml" />  -->
 <meta name="generator" content="WordPress 4.3.1" />
